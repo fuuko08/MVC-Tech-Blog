14ad0c79451bd0d5b8429f8d78da7d13b874b4cd
Selina Su <phongsu20@gmail.com>
2023-01-03 09:40:21 -0800

    app is finished

--- public/js/deletepost.js
+++ public/js/deletepost.js
@@ -1,11 +1,12 @@
 const delButton = document.querySelector('#del-post-btn');
+const postId = document.querySelector('input[name="post-id"]').value;
 
 const deleteHandler = async () => {
     const response = await fetch(`/api/post/${postId}`, {
         method: 'DELETE'
     });
     if (response.ok) {
-    document.location.replace('/');
+    document.location.replace('/dashboard');
     } else {
         alert(response.statusText);
     }
